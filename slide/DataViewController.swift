6e8219437fb3a66fef9c7e3ee8b40f529f92f909
Ryan Hart <rhart23@ryan-harts-macbook-pro.local>
2016-03-16 22:52:13 -0700

    Initial Commit

---
+++ slide/DataViewController.swift
@@ -0,0 +1,38 @@
+//
+//  DataViewController.swift
+//  slide
+//
+//  Created by Ryan Hart on 3/16/16.
+//  Copyright (c) 2016 Lion Heart Applications. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: AnyObject?
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        if let obj: AnyObject = dataObject {
+            self.dataLabel!.text = obj.description
+        } else {
+            self.dataLabel!.text = ""
+        }
+    }
+
+
+}
+
